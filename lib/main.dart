3e246d3bbdfa2440426c6067b7aead65f7d639df
Isaac Onah <iumeha@yahoo.com>
2021-01-13 15:50:20 +0100

    app title

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'My Tailor',
       theme: ThemeData(
         primarySwatch: Colors.teal,
         visualDensity: VisualDensity.adaptivePlatformDensity,
